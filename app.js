4e6d8e5e6ba2c65c6ef5f5a2c560e43692b1d49d
miley0913 <mileysalazar694@gmail.com>
2025-05-07 13:24:23 -0400

    Update app.js

--- app.js
+++ app.js
@@ -107,7 +107,258 @@ const lista = [
     type: "mágica",
     preparacion: "",
     img: "https://upload.wikimedia.org/wikipedia/commons/f/f8/Mandragora_autumnalis2.jpg"
-  }
+  },
+  {
+
+   name: "Azahar",
+
+  scientific: "Citrus aurantium",
+
+  from: "Mediterráneo",
+
+  type: "calmante espiritual",
+
+  uses: "Relaja, reconforta y da serenidad emocional profunda.",
+
+  howToUse: "Infusión de flores de azahar o aceites esenciales aromáticos.",
+
+  img: "https://aceitecsb.com/wp-content/uploads/2018/07/flor-de-azahar-717x423.jpg"
+
+},
+
+
+{
+
+  name: "Amalaki (Grosella India)",
+
+  scientific: "Phyllanthus emblica",
+
+  from: "Himalaya (India y Nepal)",
+
+  type: "medicinal y ancestral",
+
+  uses: "Fuente potente de vitamina C natural. Rejuvenecedor en medicina Ayurveda.",
+
+  howToUse: "Consumir en polvo o jugo natural para fortalecer inmunidad y vitalidad.",
+
+  img: "https://m.media-amazon.com/images/I/61SWM5qGaTL.jpg"
+
+},
+
+{
+
+  name: "Banksia",
+
+  scientific: "Banksia integrifolia",
+
+  from: "Australia",
+
+  type: "energético",
+
+  uses: "Símbolo de renovación en culturas aborígenes. Refuerza la autoestima.",
+
+  howToUse: "Usar en esencias florales para tratamientos emocionales o energía vital.",
+
+  img: "https://upload.wikimedia.org/wikipedia/commons/b/bb/Banksia_coccinea_AD_MB.jpg"
+
+},
+
+{
+
+  name: "Moringa Estrella",
+
+  scientific: "Moringa stenopetala",
+
+  from: "África Oriental (Etiopía, Kenia)",
+
+  type: "medicinal",
+
+  uses: "Antioxidante potente, regula azúcar en sangre y mejora defensas naturales.",
+
+  howToUse: "Consumir hojas frescas en ensaladas o como polvo en batidos verdes.",
+
+  img: "https://m.media-amazon.com/images/I/71bm5mBNPEL.jpg"
+
+},
+
+{
+
+  name: "Cordyceps",
+
+  scientific: "Ophiocordyceps sinensis",
+
+  from: "Himalaya",
+
+  type: "energético y espiritual",
+
+  uses: "Incrementa energía física y resistencia. Usado en rituales tibetanos antiguos.",
+
+  howToUse: "Tomar extracto en polvo o en cápsulas para aumentar vitalidad y enfoque.",
+
+  img: "https://i.herbalreality.com/wp-content/uploads/2022/01/21124416/Ophiocordyceps-sinensis-cordyceps-fungus-nature-moss-scaled.jpg"
+
+},
+  {
+name: "Pasiflora",
+
+  scientific: "Passiflora incarnata",
+
+  from: "América",
+
+  type: "calmante emocional",
+
+  uses: "Calma nervios, combate insomnio y ayuda en momentos de crisis emocional.",
+
+  howToUse: "Infusión de flores y hojas secas antes de dormir.",
+
+  img: "https://i.blogs.es/cb3035/stephane-juban-f6gia410wbm-unsplash/1366_2000.jpeg"
+
+},
+
+{
+
+      name: "Rosa Damascena",
+
+  scientific: "Rosa damascena",
+
+  from: "Oriente Medio",
+
+  type: "fortaleza emocional",
+
+  uses: "Abre el corazón, reduce duelo emocional y aporta paz interna.",
+
+  howToUse: "Infusión de pétalos o uso de aceite esencial.",
+
+  img: "https://www.lasaponaria.es/img/cms/rosa_damascena_003.jpg"
+
+},
+
+{
+
+  name: "Tilo",
+
+  scientific: "Tilia platyphyllos",
+
+  from: "Europa",
+
+  type: "tranquilidad emocional",
+
+  uses: "Calma agitación, nerviosismo y ayuda a dormir serenamente.",
+
+  howToUse: "Infusión de flores secas antes de dormir.",
+
+  img: "https://www.terzaluna.com/image/catalog/blog/tiglio/tiglio.jpg"
+
+},
+
+{
+
+  name: "Manzanilla",
+
+  scientific: "Matricaria chamomilla",
+
+  from: "Europa y Asia",
+
+  type: "calmante emocional",
+
+  uses: "Relajante ligero, reduce tensión emocional y mejora digestión nerviosa.",
+
+  howToUse: "Infusión clásica de flores secas.",
+
+  img: "https://media.admagazine.com/photos/6486ce9afa2d32627ec10acf/master/pass/manzanilla-para-plantas.jpg"
+
+
+},
+
+{
+
+   name: "Alfalfa",
+
+  scientific: "Medicago sativa",
+
+  from: "Asia Central",
+
+  type: "nutricional y energético",
+
+  uses: "Fuente rica en vitaminas y minerales, fortalece la sangre y la energía vital.",
+
+  howToUse: "Consumir brotes frescos o hacer infusión de hojas secas.",
+
+  img: "https://inaturalist-open-data.s3.amazonaws.com/photos/435568272/original.jpg"
+
+},
+
+{
+
+  name: "Malva",
+
+  scientific: "Malva sylvestris",
+
+  from: "Europa, norte de África y Asia",
+
+  type: "medicinal",
+
+  uses: "Alivia irritaciones de garganta, inflamaciones internas y heridas externas.",
+
+  howToUse: "Infusión de flores y hojas, o cataplasmas para uso externo.",
+
+  img: "https://www.thecolvinco.com/es/blog/wp-content/uploads/2017/09/tf-ma-1-01-1200x900.jpg"
+
+}, 
+  {
+    name: "Yohimbe",
+
+  scientific: "Pausinystalia johimbe",
+
+  from: "África occidental",
+
+  type: "energético",
+
+  benefits: ["circulación", "vitalidad"],
+
+  uses: "Tónico ancestral afrodisíaco, mejora la circulación y el rendimiento físico.",
+
+  img: "https://t4.ftcdn.net/jpg/06/32/31/03/360_F_632310314_fcjnUSiu8jgwUjOF0NmNoL0vuRwX5WDn.jpg"
+
+},
+
+{
+
+  name: "Ocotillo",
+
+  scientific: "Fouquieria splendens",
+
+  from: "Desierto del norte de México y EE.UU.",
+
+  type: "ancestral",
+
+  benefits: ["energía femenina", "circulación"],
+
+  uses: "Usado por pueblos nativos como regulador menstrual y para mejorar la energía de las mujeres.",
+
+  img:”https://www.ethnoherbalist.com/wp-content/uploads/2016/02/ocotillo-flower.jpg"
+
+},
+
+{
+
+  name: "Catuaba",
+
+  scientific: "Erythroxylum catuaba",
+
+  from: "Brazil (Amazonas)",
+
+  type: "energético",
+
+  benefits: ["vitalidad", "memoria"],
+
+  uses: "Tónico cerebral y afrodisíaco tradicional brasileño, mejora el ánimo, memoria y energía.",
+
+  img: "https://album.mediaset.es/eimg/2018/02/16/o5QKsmtDzyYjyiJ7Nvay01.jpg?w=1200"
+
+}
+
+  
 ];
 
 window.onload = () => {
